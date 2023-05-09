20f166321f01580180eebbbddc4e38a2aac2cf35
Sariah <sariahwandler@gmail.com>
2023-05-08 19:49:50 -0600

    finish learning 04

---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string name, string topic, string title)
+        : base(name, topic)
+    {
+        _title = title;
+    }
+    
+    public string GetWritingInfo()
+    {
+        string name = GetStudentName();
+
+        return $"{_title} by {name}";
+    }
+}
