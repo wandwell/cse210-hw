51aa68edaf24c1cfd9e6f1f1dd3cac1196fdaca1
Sariah <sariahwandler@gmail.com>
2023-05-27 23:01:07 -0600

    create classes & start writing code

---
+++ final/FinalProject/Asset.cs
@@ -0,0 +1,6 @@
+public class Asset
+{
+    private string _title;
+    private double _amount;
+    private double _interest;
+}
---
+++ final/FinalProject/DebtTracker.cs
@@ -0,0 +1,8 @@
+public class DebtTracker
+{
+    private List<Debt> _debts = new List<Debt>();
+    private double _totalMinimum;
+    private double _extraPayment;
+    private List<DateTime> _endDates = new List<DateTime>();
+    
+}
